78150693664494a378843ed1da393c03475025e7
Sushil Tiwari <susiltiwari750@gmail.com>
2024-04-19 11:25:49 +0545

    Add command for generating idus files

---
+++ apps/contrib/management/commands/generate_idus_dump_files.py
@@ -0,0 +1,20 @@
+import logging
+from django.core.management.base import BaseCommand
+from apps.contrib.tasks import generate_idus_dump_file, generate_idus_all_dump_file, generate_idus_all_disaster_dump_file
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Generate IDUS dump files'
+
+    def handle(self, *args, **options):
+        try:
+            generate_idus_dump_file()
+            generate_idus_all_dump_file()
+            generate_idus_all_disaster_dump_file()
+            logger.info('IDUS dump files generated successfully')
+
+        except Exception as exception:
+            logger.error(f'Error generating IDUS dump files: {exception}', exc_info=True)
+            raise exception
